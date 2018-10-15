4edb3123a33acc4395fae57d630476dd9e67c139
Renuka Fernando <renuka.piyumal@gmail.com>
2018-10-15 12:13:42 +0530

    add encoding for configurations

--- twitter-api-reader/python/mongo-to-csv.py
+++ twitter-api-reader/python/mongo-to-csv.py
@@ -5,11 +5,11 @@ from bson import json_util
 from pandas.io.json import json_normalize
 from pymongo import MongoClient
 
-config = ConfigParser()
-config.read('./config/reader-config.ini')
+config_parser = ConfigParser()
+config_parser.read('./config/reader-config.ini', encoding='utf-8')
 
-mongo_client = MongoClient(config.get('mongo', 'host'), int(config.get('mongo', 'port')))
-tweets = mongo_client['db'].tweets
+mongo_client = MongoClient(config_parser.get('mongo', 'host'), int(config_parser.get('mongo', 'port')))
+tweets = mongo_client[config_parser.get('mongo', 'db')][config_parser.get('mongo', 'collection')]
 data = tweets.find({})
 
 # load MongoDB data as JSON data and flatten using json_normalize
@@ -18,7 +18,7 @@ normalized = json_normalize(sanitized)
 
 normalized.to_csv(
     path_or_buf="../../data-set/data-set.csv",
-    columns=config.get('csv', 'columns').split(),
+    columns=[column.strip() for column in config_parser.get('csv', 'columns').split(',')],
     encoding="utf-8",
     index_label="instance_id"
 )
--- twitter-api-reader/python/tweets-to-mongo.py
+++ twitter-api-reader/python/tweets-to-mongo.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 from requests_oauthlib import OAuth1
 
 config_parser = ConfigParser()
-config_parser.read('./config/twitter-keys.ini')
+config_parser.read(filenames=['./config/twitter-keys.ini', './config/reader-config.ini'], encoding='utf-8')
 
 url_template = Template('https://api.twitter.com/1.1/search/tweets.json?q=$query&lang=si&tweet_mode=extended')
 
@@ -19,8 +19,7 @@ oauth = OAuth1(
     resource_owner_secret=config_parser.get('twitter', 'resource_owner_secret')
 )
 
-config_parser.read('./config/reader-config.ini')
-url = url_template.substitute(query=config_parser.get('tweet', 'query'))
+url = url_template.substitute(query=config_parser.get('tweets', 'query'))
 data = json.loads(requests.get(url, auth=oauth).text)
 
 # Mongo Client
@@ -29,7 +28,7 @@ mongo_client = MongoClient(
     port=int(config_parser.get('mongo', 'port'))
 )
 # create tweets collection
-tweets = mongo_client.db.tweets
+tweets = mongo_client[config_parser.get('mongo', 'db')][config_parser.get('mongo', 'collection')]
 
 for tweet in data['statuses']:
     tweets.insert_one(tweet)
