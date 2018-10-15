43107a219f0790de9fff9b566ed4f9ec59cb86d4
Renuka Fernando <renuka.piyumal@gmail.com>
2018-10-15 11:54:02 +0530

    add query to config file

--- twitter-api-reader/python/mongo-to-csv.py
+++ twitter-api-reader/python/mongo-to-csv.py
@@ -9,14 +9,19 @@ config = ConfigParser()
 config.read('./config/reader-config.ini')
 
 mongo_client = MongoClient(config.get('mongo', 'host'), int(config.get('mongo', 'port')))
-tweets = mongo_client.db.tweets
+tweets = mongo_client['db'].tweets
 data = tweets.find({})
 
 # load MongoDB data as JSON data and flatten using json_normalize
 sanitized = json.loads(json_util.dumps(data))
 normalized = json_normalize(sanitized)
 
-normalized.to_csv(path_or_buf="../../data-set/data-set.csv", columns=['id','user.id', 'text'], encoding="utf-8", index_label="instance_id")
+normalized.to_csv(
+    path_or_buf="../../data-set/data-set.csv",
+    columns=config.get('csv', 'columns').split(),
+    encoding="utf-8",
+    index_label="instance_id"
+)
 
 for i, j in enumerate(sanitized):
-    print(i, j['text'])
+    print(i, j['full_text'])
--- twitter-api-reader/python/tweets-to-mongo.py
+++ twitter-api-reader/python/tweets-to-mongo.py
@@ -1,24 +1,34 @@
 import json
 from configparser import ConfigParser
+from string import Template
 
 import requests
 from pymongo import MongoClient
 from requests_oauthlib import OAuth1
 
-config = ConfigParser()
-config.read('./config/reader-config.ini')
+config_parser = ConfigParser()
+config_parser.read('./config/twitter-keys.ini')
+
+url_template = Template('https://api.twitter.com/1.1/search/tweets.json?q=$query&lang=si&tweet_mode=extended')
 
 # set OAuth with twitter configurations
-oauth = OAuth1(client_key=config.get('twitter', 'client_key'),
-               client_secret=config.get('twitter', 'client_secret'),
-               resource_owner_key=config.get('twitter', 'resource_owner_key'),
-               resource_owner_secret=config.get('twitter', 'resource_owner_secret'))
+oauth = OAuth1(
+    client_key=config_parser.get('twitter', 'client_key'),
+    client_secret=config_parser.get('twitter', 'client_secret'),
+    resource_owner_key=config_parser.get('twitter', 'resource_owner_key'),
+    resource_owner_secret=config_parser.get('twitter', 'resource_owner_secret')
+)
 
-url = 'https://api.twitter.com/1.1/search/tweets.json?q=මම&lang=si'
+config_parser.read('./config/reader-config.ini')
+url = url_template.substitute(query=config_parser.get('tweet', 'query'))
 data = json.loads(requests.get(url, auth=oauth).text)
 
-# Mongo Client, create tweets collection
-mongo_client = MongoClient(config.get('mongo', 'host'), int(config.get('mongo', 'port')))
+# Mongo Client
+mongo_client = MongoClient(
+    host=config_parser.get('mongo', 'host'),
+    port=int(config_parser.get('mongo', 'port'))
+)
+# create tweets collection
 tweets = mongo_client.db.tweets
 
 for tweet in data['statuses']:
