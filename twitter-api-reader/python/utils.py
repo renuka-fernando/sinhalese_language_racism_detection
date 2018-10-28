d50aa444dd6bb035dafddb7b994fe0d52a5c5b59
Renuka Fernando <renuka.piyumal@gmail.com>
2018-10-28 12:58:21 +0530

    fix errors in lookup tweets api call

--- twitter-api-reader/python/utils.py
+++ twitter-api-reader/python/utils.py
@@ -14,17 +14,17 @@ class TweeterPremiumAPI(enum.Enum):
     full_archive = "fullarchive"
 
 
-def get_tweets_of_users(user_ids: list, oauth: OAuth1):
+def get_tweets_by_id(tweet_ids: list, oauth: OAuth1):
     """
     Get tweets of given list of user ids
-    :param user_ids: list of user ids
+    :param tweet_ids: list of tweet ids
     :param oauth: OAuth1 object for authentication
     :return: list of tweets
     """
-    if len(user_ids) >= 100:
-        raise ValueError("user id count = " + str(len(user_ids)) + ": user ids count should be 100 or less than 100")
+    if len(tweet_ids) >= 100:
+        raise ValueError("user id count = " + str(len(tweet_ids)) + ": user ids count should be 100 or less than 100")
 
-    url = 'https://api.twitter.com/1.1/statuses/lookup.json?id=' + ','.join(str(user_id) for user_id in user_ids)
+    url = 'https://api.twitter.com/1.1/statuses/lookup.json?id=' + ','.join(tweet_ids)
     return json.loads(
         requests.get(
             url=url,
@@ -40,7 +40,8 @@ def search_tweets_standard_api(query: str, oauth: OAuth1) -> list:
     :param oauth: OAuth1
     :return: tweets
     """
-    url_template = Template('https://api.twitter.com/1.1/search/tweets.json?q=$query&lang=si&tweet_mode=extended')
+    # without "tweet_mode=extended"
+    url_template = Template('https://api.twitter.com/1.1/search/tweets.json?q=$query&lang=si&count=15')
     return json.loads(
         requests.get(
             url=url_template.substitute(query=query),
@@ -76,13 +77,14 @@ def fill_truncated_tweets(truncated_tweets: list, oauth: OAuth1) -> None:
     :return: tweets with full text
     """
     for i in range(0, len(truncated_tweets) // 100):
-        full_tweets = get_tweets_of_users(user_ids=[user_id for user_id in truncated_tweets[i: 100 * (i + 1)]],
-                                          oauth=oauth)
+        full_tweets = get_tweets_by_id(tweet_ids=[tweet['id_str'] for tweet in truncated_tweets[i: 100 * (i + 1)]],
+                                       oauth=oauth)
         for j in range(100):
             truncated_tweets[i * 100 + j]['text'] = full_tweets[j]['text']
 
-    full_tweets = get_tweets_of_users(
-        user_ids=[user_id for user_id in truncated_tweets[len(truncated_tweets) // 100: len(truncated_tweets) % 100]],
+    full_tweets = get_tweets_by_id(
+        tweet_ids=[tweet['id_str'] for tweet in
+                   truncated_tweets[len(truncated_tweets) // 100: len(truncated_tweets) % 100]],
         oauth=oauth)
     for j in range(len(truncated_tweets) % 100):
         truncated_tweets[len(truncated_tweets) // 100 + j]['text'] = full_tweets[j]
