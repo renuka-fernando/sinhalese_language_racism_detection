e2210a3de4b4d950bb3bf931175ee152409f19d9
Renuka Fernando <renuka.piyumal@gmail.com>
2018-12-14 20:27:33 +0530

    remove java files

--- classifier/python/classify.py
+++ classifier/python/classify.py
@@ -10,4 +10,5 @@ for id, tweet in enumerate(data_set[:,-1]):
 
 tokenizer_cmd = ['java','SinhalaTokenizer', '1', '2', '3', '4']
 tokenizer = Popen(tokenizer_cmd, stdin=PIPE, stdout=PIPE)
-tok_text, _ = tokenizer.communicate(text)
+tok_text, _ = tokenizer.communicate(" අමාත්‍යාංශ ලේකම් එච්. එම්. ගුණසේකර මහතා හා Renuka ඔහු අමාත්‍යාංශයට කැඳවා පරීක්‌ෂා කර තිබේ".encode("utf-8"))
+print(tok_text.decode("utf-8", "ignore"))
