11bf79b5c16900005e609df8ec22eaa90c8455fd
Renuka Fernando <renuka.piyumal@gmail.com>
2018-12-15 07:56:49 +0530

    set spaces among emojis to tokenize them in text

--- sinhala-preprocessing/python/preprocessing/SinhaleseTokenizer.py
+++ sinhala-preprocessing/python/preprocessing/SinhaleseTokenizer.py
@@ -1,5 +1,7 @@
 import re
 
+import emoji
+
 
 def replace_url(text: str) -> str:
     """
@@ -7,7 +9,7 @@ def replace_url(text: str) -> str:
     :param text: text to replace urls
     :return: url removed text
     """
-    return re.sub(r'(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9A-Z\/]+)*', '', text)
+    return re.sub(r'(http://www\.|https://www\.|http://|https://)[a-z0-9]+([\-.]{1}[a-z0-9A-Z/]+)*', '', text)
 
 
 def remove_retweet_state(text: str) -> str:
@@ -16,7 +18,7 @@ def remove_retweet_state(text: str) -> str:
     :param text: text
     :return: text removed retweets state
     """
-    return re.sub(r'^RT @\w*:\ ', '', text)
+    return re.sub(r'^RT @\w*: ', '', text)
 
 
 def replace_mension(text: str) -> str:
@@ -30,10 +32,27 @@ def split_tokens(text: str) -> list:
     :return: token list
     """
     # text characters to split is from: https://github.com/madurangasiriwardena/corpus.sinhala.tools
-    return [token for token in re.split(r'[., ¸‚\"/\|\—¦”‘\'“’´´!@#$%\^&\*+\-£\?˜\(\)\[\]\{\}:;–Ê  �‪‬‏0123456789]', text) if token != ""]
+    return [token for token in
+            re.split(r'[., ¸‚\"/|—¦”‘\'“’´!@#$%^&*+\-£?˜()\[\]{\}:;–Ê  �‪‬‏0123456789]', text) if
+            token != ""]
+
+
+def set_spaces_among_emojis(text: str) -> str:
+    """
+    make spaces among emojis to tokenize them
+    :param text: text to be modified
+    :return: modified text
+    """
+    modified_text = ""
+    for c in text:
+        modified_text += c
+        if c in emoji.UNICODE_EMOJI:
+            modified_text += " "
+
+    return modified_text
 
 
 txt = "RT @sam92ky: කියවන්න..රටේ Renuka දුප්පතාට @indika27 @P0dda මිනිස්සු කුණු දාන්නේ මූහූදට නෙ.,.... ඒකයි " \
       "මෙ https://t.co/xDrwvDa3yr ඔක්කොම https://t.co/xDrwvDa3yr case. Sighhhhhhhh  😢 " \
-      "හස්බන්ඩ් උනත් එකයි නොවුනත් එකයි අපිට සෝන්ග් 😂😂😂🌺 පුකද යාලුවේ.. 😜 #RT #Help"
-print(split_tokens(replace_url(replace_mension(remove_retweet_state(txt)))))
+      "හස්බන්ඩ් උනත් {එකයි}***-+නොවුනත් [එකයි අපිට] සෝන්ග් 😂😂😂🌺 පුකද යාලුවේ.. 😜 #RT #Help"
+print(split_tokens(set_spaces_among_emojis(replace_url(replace_mension(remove_retweet_state(txt))))))
